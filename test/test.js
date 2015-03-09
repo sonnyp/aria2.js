0a204e0f90585c3ad5cd337cfbb970bde9f7beb4
Sonny Piers <sonny@fastmail.net>
2015-03-09 03:09:57 +0100

    tests

--- test/test.js
+++ test/test.js
@@ -2,65 +2,141 @@
 
   'use strict';
 
-  /* global beforeEach, describe, it */
+  /*global beforeEach, describe, it */
 
-  var assert;
   var Aria2;
   var sinon;
+  var chai;
+  var expect;
+  var sinonChai;
 
   if (typeof module !== 'undefined' && module.exports) {
     Aria2 = require('..');
-    assert = require('assert');
     sinon = require('sinon');
+    chai = require('chai');
+    sinonChai = require('sinon-chai');
+    chai.use(sinonChai);
   }
   else {
     Aria2 = global.Aria2;
-    assert = function assert(expr, msg) {
-      if (!expr) throw new Error(msg || 'failed');
-    };
+    sinon = global.sinon;
+    chai = global.chai;
+    sinonChai = global.sinonChai;
   }
 
-  var Plan = function(count, done) {
-    this.done = done;
-    this.count = count;
-  };
+  expect = chai.expect;
 
-  Plan.prototype.ok = function(expression) {
-    assert(expression);
-
-    if (this.count === 0) {
-      assert(false, 'Too many assertions called');
-    } else {
-      this.count--;
-    }
-
-    if (this.count === 0) {
-      this.done();
-    }
-  };
-
-  describe('aria2.js', function(){
+  describe('Aria2', function() {
 
     var client;
 
-    beforeEach(function(){
+    beforeEach(function() {
       client = new Aria2();
-    })
+    });
 
-    it('has a function for each methods', function(done) {
-      var plan = new Plan(Aria2.methods.length, done);
-      Aria2.methods.forEach(function(method) {
-        var spy = sinon.spy(client, 'send');
-        // console.log(spy)
-        // assert(typeof client[method] === 'function');
-        // client.send = function(m, arg) {
-        //   assert(m === method);
-        //   assert(arg === 'foo');
-        //   plan.ok(true);
-        // };
-        client[method]('foo');
-        assert(spy.withArgs(method, 'foo'));
+    it('should have a lastId number property', function() {
+      expect(client.lastId).to.be.a('number');
+      expect(client.lastId).to.equal(0);
+    });
+
+    it('should have a callback hash property', function() {
+      expect(client.callbacks).to.be.a('object');
+    });
+
+    it('should have default options properties', function() {
+      for (var i in Aria2.options) {
+        expect(client[i]).to.equal(Aria2.options[i]);
+      }
+    });
+
+    it('should have a function property for each events', function() {
+      Aria2.events.forEach(function(event) {
+        expect(client[event]).to.be.a('function');
       });
+    });
+
+    it('should have a function property for each methods', function() {
+      Aria2.methods.forEach(function(method) {
+        expect(client[method]).to.be.a('function');
+      });
+    });
+
+    it('should have a function property for each notifications', function() {
+      Aria2.notifications.forEach(function(notification) {
+        expect(client[notification]).to.be.a('function');
+      });
+    });
+
+    describe('send', function() {
+
+      it('should call onsend once with one argument', function() {
+        var spy = sinon.spy(client, 'onsend');
+        client.send('barfoo');
+        expect(spy).to.have.been.calledOnce;
+        expect(spy.args[0].length).to.equal(1);
+      });
+
+      it('should add lastId to the computed message and increment it', function() {
+        var spy = sinon.spy(client, 'onsend');
+        var lastId = client.lastId;
+        client.send('barfoo');
+        expect(spy.args[0][0]).to.have.deep.property('id', lastId)
+        expect(client.lastId).to.equal(lastId + 1);
+      });
+
+      it('should prefix the method with "aria2."', function() {
+        var spy = sinon.spy(client, 'onsend');
+        client.send('foobar');
+        expect(spy.args[0][0].method).to.equal('aria2.foobar');
+      });
+
+      it('should add json-rpc property to the computed message', function() {
+        var spy = sinon.spy(client, 'onsend');
+        client.send('foobar');
+        expect(spy.args[0][0]['json-rpc']).to.equal('2.0');
+      });
+
+      it('should compute arguments > 1 into an array and assign it to the params property', function() {
+        var spy = sinon.spy(client, 'onsend');
+        client.send('foobar', 'far', 'boo');
+        expect(spy.args[0][0].params).to.be.an('array');
+        expect(spy.args[0][0].params).to.deep.equal(['far', 'boo']);
+      });
+
+      it('should add the secret token to params if defined', function() {
+        var spy = sinon.spy(client, 'onsend');
+        client.secret = 'oo';
+        client.send('foobar', 'far', 'boo');
+        expect(spy.args[0][0].params[0]).to.equal('token:oo');
+      });
+
+    });
+
+    describe('notfications', function() {
+
+      it('should call the notification function once with correct arguments when receiving a notification', function() {
+        Aria2.notifications.forEach(function(notification) {
+          var spy = sinon.spy(client, notification);
+          var message = {'method': 'aria2.' + notification, params: ['foo', 'bar']};
+          client._onmessage(message);
+
+          expect(spy).to.have.been.calledOnce;
+          expect(spy).to.have.been.calledWith('foo', 'bar');
+        });
+      });
+
+    });
+
+    describe('methods', function() {
+
+      it('should call send once with correct arguments for each methods function', function() {
+        var spy = sinon.spy(client, 'send');
+        Aria2.methods.forEach(function(method) {
+          client[method]('foo', 'bar');
+          expect(spy).to.have.been.calledWith(method, 'foo', 'bar');
+        });
+      });
+
     });
 
   });
