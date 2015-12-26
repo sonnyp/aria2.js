09f13f0536c9a9ec8e0293fb68d2ab9a04cd1d45
Sonny Piers <sonny@fastmail.net>
2015-12-27 00:32:46 +0100

    standard

--- example/example.js
+++ example/example.js
@@ -1,55 +1,56 @@
-(function(global) {
+;(function (global) {
   'use strict'
 
   var Aria2
 
-  //Node.js, io.js
-  if (typeof module !== 'undefined' && module.exports)
+  // Node.js, io.js
+  if (typeof module !== 'undefined' && module.exports) {
     Aria2 = require('..')
-  //browsers
-  else
+  // browsers
+  } else {
     Aria2 = global.Aria2
+  }
 
-  //those are default options
+  // those are default options
   var options = {'host': 'localhost', 'port': 6800, 'secure': false}
   var aria2 = new Aria2(options)
 
-  //socket is not open yet so it will use HTTP interface
-  aria2.send('getVersion', function(err, res) {
+  // socket is not open yet so it will use HTTP interface
+  aria2.send('getVersion', function (err, res) {
     console.log('version: ', err || res)
 
-    //open socket
+    // open socket
     aria2.open()
   })
 
-  //triggered when socket is open
-  aria2.onopen = function() {
+  // triggered when socket is open
+  aria2.onopen = function () {
     console.log('OPEN')
 
-    aria2.getGlobalOption(function(err, res) {
+    aria2.getGlobalOption(function (err, res) {
       console.log('global options: ', err || res)
 
-      aria2.send('addUri', ['http://example.org/file', 'http://mirror/file'], {'dir': '/tmp'}, function(err, gid) {
+      aria2.send('addUri', ['http://example.org/file', 'http://mirror/file'], {'dir': '/tmp'}, function (err, gid) {
         console.log(err || 'gid: ' + gid)
 
-        //close socket
+        // close socket
         aria2.close()
       })
     })
   }
 
-  //triggered when socket is closed
-  aria2.onclose = function() {
+  // triggered when socket is closed
+  aria2.onclose = function () {
     console.log('CLOSED')
   }
 
-  //triggered when a message is being sent
-  aria2.onsend = function(m) {
+  // triggered when a message is being sent
+  aria2.onsend = function (m) {
     console.log('OUT:', m)
   }
 
-  //triggered when a message has been received
-  aria2.onmessage = function(m) {
+  // triggered when a message has been received
+  aria2.onmessage = function (m) {
     console.log('IN:', m)
   }
 }(this))
