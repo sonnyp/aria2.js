389979ceb4d34d6f77c2b8cd60ef45e503952a37
Sonny Piers <sonny@fastmail.net>
2015-12-27 00:32:46 +0100

    standard

--- bin/console.js
+++ bin/console.js
@@ -3,21 +3,21 @@
 var readline = require('readline')
 var Aria2 = require('..')
 
-module.exports = function(cli, options) {
+module.exports = function (cli, options) {
   var debug = require('./debug')(cli)
 
   var client = new Aria2(options)
-  client.onsend = function(m) {
+  client.onsend = function (m) {
     debug('OUT', m)
   }
-  client.onmessage = function(m) {
+  client.onmessage = function (m) {
     debug('IN', m)
     if (m.id === undefined) {
       console.log(m)
     }
   }
   debug('CONNECTING')
-  client.open(function(err) {
+  client.open(function (err) {
     if (err) {
       console.error(err)
       process.exit(1)
@@ -27,15 +27,15 @@ module.exports = function(cli, options) {
 
     var rl = readline.createInterface({
       'input': process.stdin,
-      'output': process.stdout,
+      'output': process.stdout
     })
     rl.setPrompt('aria2rpc ≻ ')
     rl.prompt()
-    rl.on('line', function(line) {
+    rl.on('line', function (line) {
       line = line.trim()
       if (!line) return rl.prompt()
       var params = line.split(' ')
-      var cb = function(err, res) {
+      var cb = function (err, res) {
         if (err) console.error(err)
         else console.log(res)
         rl.prompt()
@@ -45,9 +45,9 @@ module.exports = function(cli, options) {
 
       client.send.apply(client, args)
     })
-    rl.on('close', function() {
+    rl.on('close', function () {
       debug('CLOSING')
-      client.close(function() {
+      client.close(function () {
         debug('CLOSED')
         process.exit(0)
       })
--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,7 @@
-(function(global) {
+(function (global) {
   'use strict'
+
+  /* global describe, it, beforeEach */
 
   var Aria2
   var sinon
@@ -13,8 +15,7 @@
     chai = require('chai')
     sinonChai = require('sinon-chai')
     chai.use(sinonChai)
-  }
-  else {
+  } else {
     Aria2 = global.Aria2
     sinon = global.sinon
     chai = global.chai
@@ -23,71 +24,67 @@
 
   expect = chai.expect
 
-  describe('Aria2', function() {
-
+  describe('Aria2', function () {
     var client
 
-    beforeEach(function() {
+    beforeEach(function () {
       client = new Aria2()
       client.http = sinon.stub()
     })
 
-    it('should be an instance of Aria2', function() {
+    it('should be an instance of Aria2', function () {
       expect(client).to.be.an.instanceOf(Aria2)
     })
 
-    describe('instance properties', function() {
-
-      it('should have a lastId number property', function() {
+    describe('instance properties', function () {
+      it('should have a lastId number property', function () {
         expect(client.lastId).to.be.a('number')
         expect(client.lastId).to.equal(0)
       })
 
-      it('should have a callback hash property', function() {
+      it('should have a callback hash property', function () {
         expect(client.callbacks).to.be.a('object')
       })
 
-      it('should have default options properties', function() {
+      it('should have default options properties', function () {
         for (var i in Aria2.options) {
           expect(client[i]).to.equal(Aria2.options[i])
         }
       })
 
-      it('should have a function property for each events', function() {
-        Aria2.events.forEach(function(event) {
+      it('should have a function property for each events', function () {
+        Aria2.events.forEach(function (event) {
           expect(client[event]).to.be.a('function')
         })
       })
 
-      it('should have a function property for each methods', function() {
-        Aria2.methods.forEach(function(method) {
+      it('should have a function property for each methods', function () {
+        Aria2.methods.forEach(function (method) {
           expect(client[method]).to.be.a('function')
         })
       })
 
-      it('should have a function property for each notifications', function() {
-        Aria2.notifications.forEach(function(notification) {
+      it('should have a function property for each notifications', function () {
+        Aria2.notifications.forEach(function (notification) {
           expect(client[notification]).to.be.a('function')
         })
       })
     })
 
-    describe('instance methods', function() {
-
-      describe('send', function() {
-
-        it('throws a TypeError if method is not a string', function() {
+    describe('instance methods', function () {
+      describe('send', function () {
+        it('throws a TypeError if method is not a string', function () {
           expect(client.send.bind(client, null)).to.throw(TypeError)
         })
 
-        it('should call onsend once with one argument', function() {
+        it('should call onsend once with one argument', function () {
           var spy = sinon.spy(client, 'onsend')
           client.send('barfoo')
           expect(spy).to.have.been.calledOnce
           expect(spy.args[0].length).to.equal(1)
         })
 
-        it('should add lastId to the computed message and increment it', function() {
+        it('should add lastId to the computed message and increment it', function () {
           var spy = sinon.spy(client, 'onsend')
           var lastId = client.lastId
           client.send('barfoo')
@@ -95,36 +92,48 @@
           expect(client.lastId).to.equal(lastId + 1)
         })
 
-        it('should prefix the method with "aria2."', function() {
+        it('should prefix the method with "aria2."', function () {
           var spy = sinon.spy(client, 'onsend')
           client.send('foobar')
           expect(spy.args[0][0].method).to.equal('aria2.foobar')
         })
 
-        it('should add json-rpc property to the computed message', function() {
+        it('shouldn\'t prefix the method with "aria2." if it starts with "system."', function () {
+          var spy = sinon.spy(client, 'onsend')
+          client.send('system.foobar')
+          expect(spy.args[0][0].method).to.equal('system.foobar')
+        })
+
+        it('shouldn\'t prefix the method with "aria2." if it starts with "aria2."', function () {
+          var spy = sinon.spy(client, 'onsend')
+          client.send('aria2.foobar')
+          expect(spy.args[0][0].method).to.equal('aria2.foobar')
+        })
+
+        it('should add json-rpc property to the computed message', function () {
           var spy = sinon.spy(client, 'onsend')
           client.send('foobar')
           expect(spy.args[0][0]['json-rpc']).to.equal('2.0')
         })
 
-        it('should compute arguments > 1 into an array and assign it to the params property', function() {
+        it('should compute arguments > 1 into an array and assign it to the params property', function () {
           var spy = sinon.spy(client, 'onsend')
           client.send('foobar', 'far', 'boo')
           expect(spy.args[0][0].params).to.be.an('array')
           expect(spy.args[0][0].params).to.deep.equal(['far', 'boo'])
         })
 
-        it('should add the secret token to params if defined', function() {
+        it('should add the secret token to params if defined', function () {
           var spy = sinon.spy(client, 'onsend')
           client.secret = 'oo'
           client.send('foobar', 'far', 'boo')
           expect(spy.args[0][0].params[0]).to.equal('token:oo')
         })
 
-        it('should add the function argument to callbacks', function() {
+        it('should add the function argument to callbacks', function () {
           var spy = sinon.spy(client, 'onsend')
           client.secret = 'oo'
-          var cb = function() {}
+          var cb = function () {}
           client.send('foobar', 'foo', 'bar', cb)
           expect(spy.args[0][0].params).to.be.an('array')
           expect(spy.args[0][0].params).to.deep.equal(['token:oo', 'foo', 'bar'])
@@ -133,10 +142,9 @@
         })
       })
 
-      describe('_onmessage', function() {
-
-        it('should call the notification function once with correct arguments when receiving a notification', function() {
-          Aria2.notifications.forEach(function(notification) {
+      describe('_onmessage', function () {
+        it('should call the notification function once with correct arguments when receiving a notification', function () {
+          Aria2.notifications.forEach(function (notification) {
             var spy = sinon.spy(client, notification)
             var message = {'method': 'aria2.' + notification, 'params': ['foo', 'bar']}
             client._onmessage(message)
@@ -146,41 +154,41 @@
           })
         })
 
-        it('should call the callback of a request when receiving a response', function() {
+        it('should call the callback of a request when receiving a response', function () {
           var spySend = sinon.spy(client, 'onsend')
           var callback = sinon.spy()
           client.send('foobar', callback)
           var id = spySend.args[0][0].id
           expect(client.callbacks[id]).to.equal(callback)
-          var message = {"method": "aria2.foobar", "id": id}
+          var message = {'method': 'aria2.foobar', 'id': id}
           client._onmessage(message)
 
           expect(callback).to.have.been.calledOnce
         })
 
-        it('should call and delete the callback of a request with error when receiving a response with error', function() {
+        it('should call and delete the callback of a request with error when receiving a response with error', function () {
           var spySend = sinon.spy(client, 'onsend')
           var callback = sinon.spy()
           client.send('foobar', callback)
           var id = spySend.args[0][0].id
           expect(client.callbacks[id]).to.equal(callback)
-          var message = {"method": "aria2.foobar", "id": id, "error": "whatever"}
+          var message = {'method': 'aria2.foobar', 'id': id, 'error': 'whatever'}
           client._onmessage(message)
 
           expect(callback).to.have.been.calledWith('whatever')
           expect(client.callbacks[id]).to.equal(undefined)
         })
 
-        it('should call and delete the callback of a request with result when receiving a response with result', function() {
+        it('should call and delete the callback of a request with result when receiving a response with result', function () {
           var spySend = sinon.spy(client, 'onsend')
           var callback = sinon.spy()
           client.send('foobar', callback)
           var id = spySend.args[0][0].id
           expect(client.callbacks[id]).to.equal(callback)
           var message = {
-            "id": id,
-            "method": "aria2.foobar",
-            "result": "foobar",
+            'id': id,
+            'method': 'aria2.foobar',
+            'result': 'foobar'
           }
           client._onmessage(message)
 
@@ -189,12 +197,11 @@
         })
       })
 
-      describe('aria2 methods', function() {
-
-        it('should call send once with correct arguments for each methods function', function() {
+      describe('aria2 methods', function () {
+        it('should call send once with correct arguments for each methods function', function () {
           var spy = sinon.spy(client, 'send')
-          var cb = function() {}
-          Aria2.methods.forEach(function(method) {
+          var cb = function () {}
+          Aria2.methods.forEach(function (method) {
             client[method]('foo', 'bar', cb)
             expect(spy).to.have.been.calledWith(method, 'foo', 'bar', cb)
           })
