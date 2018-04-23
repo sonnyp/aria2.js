7cf2df2339a724f6e6fabdd2933eaaeaf933e28f
Sonny Piers <sonny@fastmail.net>
2018-04-23 18:49:25 +0200

    upgrade dependencies

--- bin/console.js
+++ bin/console.js
@@ -1,56 +1,56 @@
-'use strict'
+"use strict";
 
-var readline = require('readline')
-var Aria2 = require('..')
+var readline = require("readline");
+var Aria2 = require("..");
 
-module.exports = function (cli, options) {
-  var debug = require('./debug')(cli)
+module.exports = function(cli, options) {
+  var debug = require("./debug")(cli);
 
-  var client = new Aria2(options)
-  client.onsend = function (m) {
-    debug('OUT', m)
-  }
-  client.onmessage = function (m) {
-    debug('IN', m)
+  var client = new Aria2(options);
+  client.onsend = function(m) {
+    debug("OUT", m);
+  };
+  client.onmessage = function(m) {
+    debug("IN", m);
     if (m.id === undefined) {
-      console.log(m)
+      console.log(m);
     }
-  }
-  debug('CONNECTING')
-  client.open(function (err) {
+  };
+  debug("CONNECTING");
+  client.open(function(err) {
     if (err) {
-      console.error(err)
-      process.exit(1)
+      console.error(err);
+      process.exit(1);
     }
 
-    debug('CONNECTED')
+    debug("CONNECTED");
 
     var rl = readline.createInterface({
-      'input': process.stdin,
-      'output': process.stdout
-    })
-    rl.setPrompt('aria2rpc ≻ ')
-    rl.prompt()
-    rl.on('line', function (line) {
-      line = line.trim()
-      if (!line) return rl.prompt()
-      var params = line.split(' ')
-      var cb = function (err, res) {
-        if (err) console.error(err)
-        else console.log(res)
-        rl.prompt()
-      }
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.setPrompt("aria2rpc ≻ ");
+    rl.prompt();
+    rl.on("line", function(line) {
+      line = line.trim();
+      if (!line) return rl.prompt();
+      var params = line.split(" ");
+      var cb = function(err, res) {
+        if (err) console.error(err);
+        else console.log(res);
+        rl.prompt();
+      };
 
-      var args = params.concat(cb)
+      var args = params.concat(cb);
 
-      client.send.apply(client, args)
-    })
-    rl.on('close', function () {
-      debug('CLOSING')
-      client.close(function () {
-        debug('CLOSED')
-        process.exit(0)
-      })
-    })
-  })
-}
+      client.send.apply(client, args);
+    });
+    rl.on("close", function() {
+      debug("CLOSING");
+      client.close(function() {
+        debug("CLOSED");
+        process.exit(0);
+      });
+    });
+  });
+};
